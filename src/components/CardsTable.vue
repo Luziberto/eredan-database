27d67b6b0d2b2f4934a17547c83faafb3bbbf4ad
Luziberto Mendes <luzibertomendesjunior@hotmail.com>
2022-12-10 16:27:40 -0300

    add table

--- src/components/CardsTable.vue
+++ src/components/CardsTable.vue
@@ -1,42 +1,38 @@
 <template>
-  <div class="flex flex-col">
+  <div class="flex flex-col bg-gray-300">
     <div class="grid grid-flow-row -my-2 overflow-x-auto auto-rows-max">
       <div class="overflow-hidden border-b border-gray-200 shadow lg:rounded-lg">
-        <table class="min-w-full divide-y divide-gray-200">
-          <thead class="min-w-full divide-y divide-gray-200">
-          </thead>
-          <tbody class="bg-white divide-y divide-gray-200 grid grid-cols-2 lg:grid-cols-7">
+        <table class="w-full">
+          <tbody class="divide-y divide-gray-200">
             <tr
-              v-for="card in cards"
-              :key="`card-${card.id}`"
-              class="flex justify-between cursor-pointer hover:bg-gray-100"
-              @click="openModal(card)"
+              v-for="line in Math.ceil(cards.length / TABLE_CONFIG.ITEMS_PER_LINE)"
+              :class="`grid grid-cols-2 lg:grid-cols-${TABLE_CONFIG.ITEMS_PER_LINE}`"
+              :key="`line-${line}`"
             >
-              <td class="px-4 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap flex-1">
-                <div class="flex flex-col items-center justify-center">
-                  <img
-                    :src="`http://static.eredan.com/cards/web_big/${translate.IMG_FOLDER}/${card.filename}.png`"
-                    alt=""
-                  >
-                  <span
-                    className="lg:hidden w-40 pt-2 whitespace-nowrap font-bold text-sm lg:text-lg leading-5 text-gray-900 whitespace-no-wrap truncate"
-                  >
-                    {{ card.labels[translate.LANGUAGE_ABBREVIATION as keyof CardLabel].name }}
-                  </span>
-                </div>
+              <td
+                v-for="index in (
+                  (line * TABLE_CONFIG.ITEMS_PER_LINE) > cards.length ?
+                    line * TABLE_CONFIG.ITEMS_PER_LINE - cards.length :
+                    TABLE_CONFIG.ITEMS_PER_LINE
+                )"
+                :key="`card-${getCardIndex(index, line)}`"
+                class="px-4 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap cursor-pointer hover:bg-gray-100"
+              >
+                <div class="h-96"></div>
+                {{ getCardIndex(index, line) }}
+
               </td>
             </tr>
           </tbody>
-          <CardListObserver
-            v-if="activeInfiniteScroll"
-            @more-data="pushCards"
-            @finish-data="activeInfiniteScroll = false"
-          />
-          <Loading v-show="activeInfiniteScroll" />
         </table>
       </div>
     </div>
-
+    <CardListObserver
+      v-if="activeInfiniteScroll"
+      @more-data="pushCards"
+      @finish-data="activeInfiniteScroll = false"
+    />
+    <Loading v-show="activeInfiniteScroll" />
   </div>
 </template>
 
@@ -48,6 +44,8 @@ import Loading from "@/components/global/LoadingAnimation.vue"
 import { reactive, ref } from "vue"
 import { useLocaleStore } from "@/store/locale"
 import { storeToRefs } from "pinia"
+import { Orientation } from "@/constants/OrientationConstants"
+import { TABLE_CONFIG } from "@/constants/CardConstants"
 
 const cards = reactive<Card[]>([])
 const activeInfiniteScroll = ref<boolean>(true)
@@ -56,13 +54,28 @@ const localeStore = useLocaleStore()
 const { translate } = storeToRefs(localeStore)
 
 const emit = defineEmits<{
-  (e: "openModal", card: Card): void
+  (e: "openModal", modalValue: boolean, card: Card, orientation: string): void
+  (e: "closeModal", modalValue: boolean, card: Card): void
 }>()
 
-const openModal = (card: Card) => {
-  emit("openModal", card)
+const openModal = (card: Card, e: MouseEvent) => {
+  const rect = (e.target as HTMLElement).getBoundingClientRect()
+  const x = e.clientX
+  const orientation = (window.innerWidth / 2) > x ? Orientation.RIGHT : Orientation.LEFT
+
+  setTimeout(() => {
+    emit("openModal", true, card, orientation)
+  }, 100);
 }
 
+const closeModal = () => {
+  console.log('fechar modal')
+  emit("closeModal", false, {} as Card)
+}
+
+const getCardIndex = (index: number, line: number): number => {
+  return (index - 1 + (line - 1) * TABLE_CONFIG.ITEMS_PER_LINE)
+}
 const pushCards = (newCards: Card[]) => {
   cards.push(...newCards)
 }
@@ -71,6 +84,7 @@ const refreshCards = (newCards: Card[], infiniteScrollState: boolean) => {
   cards.splice(0, cards.length)
   activeInfiniteScroll.value = infiniteScrollState
   pushCards(newCards)
+  console.log(cards)
 }
 
 defineExpose({ refreshCards })
